1ed1382a4825c16d0ea550c48afcee99cc31ba43
bananaSec <bananasec.twhh@gmail.com>
2019-12-29 17:28:30 +0530

    getitlesof

---
+++ getitlesof.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# To get titles from list of URLs from file
+# Usage: ./getitles.sh urls.txt
+path=$2
+host=$1
+while IFS= read -r line1
+do
+  while IFS= read -r line2
+  do
+    line = $line1.$line2
+    echo -n $line "        "
+    wget -qO- $line | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si'
+  done < "$path" 
+done < "$host"
